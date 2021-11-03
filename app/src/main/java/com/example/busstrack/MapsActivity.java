df262a3d122ac7110865b1d8e9c82d096f3bf90a
SasAndrei <sasmiresanandrei@gmail.com>
2021-11-03 13:23:53 +0200

    New Key

--- app/src/main/java/com/example/busstrack/MapsActivity.java
+++ app/src/main/java/com/example/busstrack/MapsActivity.java
@@ -34,7 +34,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * we just add a marker near cluj, Australia.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -43,9 +43,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in cluj and move the camera
+        LatLng cluj = new LatLng(46.7712, 23.6236);
+        mMap.addMarker(new MarkerOptions().position(cluj).title("Marker in Cluj-Napoca"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(cluj));
     }
 }

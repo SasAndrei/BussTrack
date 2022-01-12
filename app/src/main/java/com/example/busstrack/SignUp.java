624e9fa9e1c67d216988a1b7bab80a708411fd07
SasAndrei <sasmiresanandrei@gmail.com>
2022-01-12 11:09:13 +0200

    new changes

--- app/src/main/java/com/example/busstrack/SignUp.java
+++ app/src/main/java/com/example/busstrack/SignUp.java
@@ -1,4 +1,29 @@
 package com.example.busstrack;
 
-public class SignUp {
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignUp extends AppCompatActivity {
+
+    TextView alreadyHaveAccount;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+    }
 }
